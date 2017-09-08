e1c1b48aabf419448be21a85a3512f18d04c64ab
Timothy Hoang <timh1203@gmail.com>
2017-09-07 20:15:43 -0500

    Add javascript logic for new quote and sharing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
-.introduction {
-	
-}
+$(document).ready(function() {
+    var quote;
+    var author;
+    getQuote(); // Initial Quote
+    
+    function getQuote(){    
+      var forismaticAPI = "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?";  
+      
+      $.getJSON(forismaticAPI, function(data) {
+        quote = data.quoteText;
+        author = data.quoteAuthor;
+         $("#quote").text(data.quoteText);
+         $("#author").text("-"+data.quoteAuthor);
+      });   
+    }; //getQuote function
+  
+    $("#newQuote").on("click", function(){
+        getQuote();
+    });
+
+    $("#twitter-share").on("click", function(){
+        window.open("https://twitter.com/intent/tweet?text=" + quote + "-" + author + " @traveldev" + " https://goo.gl/CKSfrt");
+    });
+
+    $("#facebook-share").on("click", function(){
+        window.open("https://www.facebook.com/sharer/sharer.php?u=https://timh1203.github.io/random-quote-machine/");
+    });
+}); //docready
